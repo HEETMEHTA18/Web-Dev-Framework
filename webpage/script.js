7cc22a30f54ed995943b2cc1c20d4c9188adac11
Heet Mehta <heetmehta18125@gmail.com>
2025-07-10 18:45:34 +0530

    Js started

---
+++ webpage/script.js
@@ -0,0 +1,12 @@
+
+    function myfunc()
+    {
+        var str ="You clicked the wrong button"
+     var str1=420;
+
+    document.write('<b><br/>'+str+'<br/>');
+    // alert(str);
+    documnt.write('hello world!...<br/>');
+    documnt.reade(str);
+    console.log(str);
+    }
