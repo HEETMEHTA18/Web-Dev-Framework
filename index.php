50042b654f1a5f96a770eceab5ce488ae6f10c99
Heet Mehta <heetmehta18125@gmail.com>
2025-08-02 18:42:28 +0530

    inital commit

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>HEET MEHTA - PHP</title>
+    <title>HEET MEHTA - PHP</title>
 </head>
 <body>
-<h1>HEET MEHTA  in PHP</h1>
-<p>The SHA256 hash of "HEET MEHTA " is</p>
-<pre>
+    <h1>HEET MEHTA in PHP</h1>
+    <p>The SHA256 hash of "HEET MEHTA " is</p>
+    <pre>
 <?php
 print hash('sha256', 'HEET MEHTA ');
 ?>
-</pre>
-<p>ASCII ART:</p>
-<pre>
-    **
-   * *
-  * *
- *******
-* *
-* *
-</pre>
-<a href="check.php">Click here to check the error setting</a>
-<br/>
-<a href="fail.php">Click here to cause a traceback</a>
+    </pre>
+    <p>ASCII ART:</p>
+    <pre>
+*     *
+*     *
+*     *
+*******
+*     *
+*     *
+*     *
+    </pre>
+    <a href="check.php">Click here to check the error setting</a>
+    <br/>
+    <a href="fail.php">Click here to cause a traceback</a>
 </body>
 </html>
